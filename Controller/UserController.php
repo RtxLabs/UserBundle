57a69262e96f59a04a01dee6efab851b1aaf1701
Marcel Baust <mb@i42.de>
2014-04-30 15:15:31 +0200

    ROC-743 "Letzter Login" wird nicht korrekt angezeigt

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -13,7 +13,7 @@ use RtxLabs\UserBundle\Model\UserManager;
 class UserController extends RestController
 {
     private $whitelist = array("id", "firstname", "lastname", "email", "username",
-        "passwordRequired", "plainPassword", "passwordRepeat", "locale", "active");
+        "passwordRequired", "plainPassword", "passwordRepeat", "locale", "active", "lastLogin");
 
     /**
      * @Route("/user", name="rtxlabs_userbundle_user_list", requirements={"_method"="GET"}, options={"expose"="true"})
